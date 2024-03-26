fc7b247eeb7eb68098413a845634344de70d1825
Bespi123 <b_antonioesp@hotmail.com>
2024-03-26 16:18:32 -0500

    Add a sintonization function

--- simplePID/myObjectiveFunction.m
+++ simplePID/myObjectiveFunction.m
@@ -1,4 +1,4 @@
-function J=myObjectiveFunction(x)
+function J=myObjectiveFunction(k)
 % Template for Creating an Objective Function Using a Simulink Block Diagram
 % x contains all the controller variables of the problem in a column vector.
 % Acknoledgment: 
@@ -10,12 +10,16 @@ function J=myObjectiveFunction(x)
 % Separate the variables into their appropriate names, according to the
 % problem at hand. These variables correspond to the elements of x, which
 % must be n different elements.
-PID_kp=x(1); PID_kd=x(2); PID_ki=x(3);     %Pid gains 
+x.PID.Kp = k(1); x.PID.Ki = k(2); x.PID.Kd = k(3);     %Pid gains 
 
 %%%%%%%%%%%%%%%%%%%     SECTION 2: Conditions         %%%%%%%%%%%%%%%%%%%%%
 % Operating conditions of the problem. If the problem has parameters or 
 % data that you need to add, place them in this section.
-xcoilParameters; %call plant variables
+%%%x-coil
+x.Kp  = 3639.2 ;                 
+x.Tp1 = 108.65 ;                
+x.Tp2 = 0.52435;               
+x.Tz  = 24.199 ; %call plant variables
 
 %%%%%%%%%%%%%%%%%%%     SECTION 3: Pre-Calculations   %%%%%%%%%%%%%%%%%%%%%
 % Pre-calculations
@@ -26,15 +30,28 @@ xcoilParameters; %call plant variables
 % Simulink is called to simulate the process of interest and calculate the
 % simulation outputs
 % WARNING!!! The model name must be changed in the following line
-salidas=sim('simplePID','SrcWorkspace','current');
+try
+   salidas=sim('xcoilSintonization','SrcWorkspace','current');
+   stable = 1;
+catch exception
+   
+   if strcmp(exception.identifier,'Simulink:Engine:DerivNotFinite')
+    stable = 0;
+    disp('System No stable')
+   else
+       disp('Otro error')
+   end
+end
+
+if(stable == 1)
 % The simulation outputs are divided into two groups, the time and the
 % outputs themselves
-yout=salidas.get('yout');
-t=salidas.get('tout');
+yout = salidas.get('yout');
+t    = salidas.get('tout');
 
 % It is common to have the system error as out1, the controller output u as
 % out2, and the output y as out3
-e=yout(:,1); u=yout(:,2); y=yout(:,3);%Define erro, control signal and output
+u=yout(:,1); y=yout(:,2); e=yout(:,3);%Define error, control signal and output
 
 %%%%%%%%%%%%%%%%%%% SECTION 5: Calculate the Fitness  %%%%%%%%%%%%%%%%%%%%%%
 % The response of the process is now analyzed. If the input is not a step, 
@@ -45,39 +62,29 @@ e=yout(:,1); u=yout(:,2); y=yout(:,3);%Define erro, control signal and output
 %digamos que se desea que el tiempo de establecimiento sea un valor
 %específico
 desired_setlement_time = 376;
-% Define the settling time threshold (e.g., 2%)
-threshold_setlement_time = 0.02; 
-% Find the final value of the output
-final_value = y(end);
-% Identify the settling time index
-settling_time_idx = find( ~(e < threshold_setlement_time*final_value), 1, 'last');
-% Calculate the current settling time
-setling_time = t(settling_time_idx);
-setlement_time_error = abs(desired_setlement_time-setling_time);
+desired_rising_time    = 80;
 
-%----RisingTime
-desired_rising_time = 80;
-% Define the settling time threshold (e.g., 2%)
-threshold_rising_time =  0.5;
-% Identify the settling time index
-rising_time_idx = find( ~(e < threshold_rising_time*final_value), 1, 'last');
-% Calculate the settling time
-rising_time = t(rising_time_idx);
-rising_time_error = abs(desired_rising_time-rising_time);
+% calculate rising time
+tr = calculateRissingTime(e, t, 50/100);
+d_tr = abs(desired_rising_time-tr);
+
+% Calculate settlement time
+ts = calculateSettlementTime(e, t, 5/100);
+d_ts = abs(desired_setlement_time-ts);
 
 %-----índices de error integral --------
 %itse=trapz(t,t.*e.^2);
-itse=trapz(t,e.^2);
+itse = trapz(t,e.^2);
 
-%-----
-overshoot = max(y);
-a = overshoot - 1;
-if a > 0
-    overshoot_index = a;
-else
-    overshoot_index = 0;
-end
-%umax = max(u);
+% % %-----
+% % overshoot = max(y);
+% % a = overshoot - 1;
+% % if a > 0
+% %     overshoot_index = a;
+% % else
+% %     overshoot_index = 0;
+% % end
+
 uwork = trapz(t,u.^2);
 
 %%%-----Elección del índice deseado como fitness-------
@@ -85,4 +92,13 @@ uwork = trapz(t,u.^2);
 %%%J=itse+overshoot+umax+uwork+timeWorking;
 %%%J=itse+setlement_time_error+overshoot+rising_time_error;
 %%%J=itse+setlement_time_error+rising_time_error;
-J=0.2*itse+0.3*rising_time_error^2+0.3*setlement_time_error^2+0.2*overshoot_index^2+0.2*uwork;
+if isempty(tr) || isempty(ts)
+    J = 5E4;
+else
+    J = d_tr+d_ts+itse+uwork; %%%High value of J
+end
+else
+    J = 5E4;
+end
+
+end
--- simplePID/optimizationProgram.m
+++ simplePID/optimizationProgram.m
@@ -1,16 +1,19 @@
 no_var=3;  %number of variables
 lb = [0 0 0]; % lower bound
 up = [0.01 0.01 0.01]; % high bound
-%initial = [0.00664884785499449	0.00321464749562130	1.16161059495086e-05];
-initial = [ 1.4605e-04 0 1.0648e-05];
+%initial = [1.479699757805321e-04 1.513933366758091e-05	0.009330283036400];
+initial = [6.446234579382160e-04, 1.265158550081826e-04, 4.920959472656250e-04];
 
 %GA OPTIONS
-ga_opt = gaoptimset('Display','off','Generations',25,'PopulationSize',100, ...
+%try
+ga_opt = gaoptimset('Display','off','Generations',50,'PopulationSize',200, ...
     'InitialPopulation',initial,'PlotFcns',@gaplotbestf);
 obj_fun = @(k)myObjectiveFunction(k);
 
 [k,bestblk] = ga((obj_fun),no_var,[],[],[],[],lb,up,[],ga_opt);
-
-opt_kp = k(1);
-opt_ki = k(3);
-opt_kd = k(2);
+%%catch exception
+%    disp('Error');
+%end
+%opt_kp = k(1);
+%opt_ki = k(3);
+%opt_kd = k(2);
